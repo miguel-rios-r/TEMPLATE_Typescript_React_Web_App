b3a4826b6abfd508079a1feb9021cfa722bd0bba
Miguel Rios R <mgl.rios.r@gmail.com>
2022-07-31 15:13:59 -0500

    Add typescript to basic template

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Template for ReactJS project
-Starter template for React Web Clients [ReactJS, Webpack, Babel  and React Router Dom]
+# Template for Typescripst ReactJS project
+Starter template for React Web Clients [ReactJS, Typescript, Webpack, Babel  and React Router Dom]
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: path.join(__dirname, "src", "App.js"),
+  entry: path.join(__dirname, "src", "App.tsx"),
   output: {
     path:path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
